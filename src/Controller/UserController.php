f635772d19a9625aeabd7b25151ac57e0ab9a0f8
Nicolas DENIS <me@scoopandrun.ovh>
2024-05-09 15:44:45 +0200

    Add user create and deletion

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use App\DTO\PaginationDTO;
 use App\Entity\User;
 use App\Service\UserService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route("/api/users", name="user.")
@@ -49,5 +51,91 @@ class UserController extends AbstractController
                 'pagination' => $paginationDTO,
             ]
         );
+    }
+
+    /**
+     * @Route("/{id}", name=".show", methods={"GET"})
+     */
+    public function show(
+        User $user,
+        SerializerInterface $serializer
+    ): JsonResponse {
+        if (!$this->user instanceof User) {
+            return $this->json(null, 401);
+        }
+
+        if ($user->getCompany() !== $this->user->getCompany()) {
+            return $this->json(
+                [
+                    "message" => "You cannot get information from another company's users"
+                ],
+                403
+            );
+        }
+
+        return $this->json(
+            $user,
+            200,
+            [],
+            ['groups' => 'user.show']
+        );
+    }
+
+    /**
+     * @Route("/", name=".create", methods={"POST"})
+     */
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ): JsonResponse {
+        if (!$this->user instanceof User) {
+            return $this->json(null, 401);
+        }
+
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            return $this->json($errors, 400);
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json(
+            $user,
+            201,
+            [],
+            ['groups' => 'user.show']
+        );
+    }
+
+    /**
+     * @Route("/{id}", name=".delete", methods={"DELETE"})
+     */
+    public function delete(
+        User $user,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        if (!$this->user instanceof User) {
+            return $this->json(null, 401);
+        }
+
+        if ($user->getCompany() !== $this->user->getCompany()) {
+            return $this->json(
+                [
+                    "message" => "You cannot delete another company's user"
+                ],
+                403
+            );
+        }
+
+        $entityManager->remove($user, true);
+        $entityManager->flush();
+
+        return $this->json(null, 204);
     }
 }
---
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -0,0 +1,63 @@
+<?php
+
+// This class normalizes and denormalizes the User entity
+// Path: src/Serializer/Normalizer/UserNormalizer.php
+
+namespace App\Serializer\Normalizer;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class UserNormalizer implements ContextAwareNormalizerInterface, DenormalizerInterface
+{
+    private $normalizer;
+    private $security;
+    private $passwordHasher;
+
+    public function __construct(
+        ObjectNormalizer $normalizer,
+        Security $security,
+        UserPasswordHasherInterface $passwordHasher
+    ) {
+        $this->normalizer = $normalizer;
+        $this->security = $security;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function normalize($user, ?string $format = null, array $context = [])
+    {
+        $data = $this->normalizer->normalize($user, $format, $context);
+
+        // Here, add, edit, or delete some data:
+        $data["company"] = $user->getCompany()->getName();
+
+        return $data;
+    }
+
+    public function denormalize($data, string $type, ?string $format = null, array $context = [])
+    {
+        /** @var User */
+        $currentUser = $this->security->getUser();
+
+        $user = new User();
+        $user->setEmail($data['email'] ?? "");
+        $user->setCompany($currentUser->getCompany());
+        $user->setPassword($this->passwordHasher->hashPassword($user, $data['password'] ?? ""));
+
+        return $user;
+    }
+
+    public function supportsNormalization($data, ?string $format = null, array $context = [])
+    {
+        return $data instanceof User;
+    }
+
+    public function supportsDenormalization($data, string $type, ?string $format = null)
+    {
+        return $type === User::class;
+    }
+}
