1ea47536e50deefb999d277989399643c66a38a5
Nicolas DENIS <me@scoopandrun.ovh>
2024-05-09 15:45:59 +0200

    Refactor route information

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api", name="home.")
+ */
 class HomeController extends AbstractController
 {
     /**
